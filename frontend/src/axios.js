f88801d7deda55b7d014a5eca5e950961ea24788
Kevin <>
2021-03-19 18:03:20 -0400

    Fix minor bugs and clean up axios.js file

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://hinder-backend.herokuapp.com/'
+    baseURL: 'https://hinder-backend.herokuapp.com'
 })
 
 export default instance;
